f16d6f07b7590be5b55f8878903690a3453f9683
Myrat204209 <myrat.annamuhammedov2002@gmail.com>
2026-08-26 11:13:20 +0500

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Azat Japarov on 26.08.26.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
